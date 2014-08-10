bbe072c36e145cff7f5cec962e997c7280a69382
m0nd4 <9nuto0x@gmail.com>
2014-08-10 19:42:18 -0300

    Plot 3

---
+++ plot3.R
@@ -0,0 +1,22 @@
+## Read data.
+datos <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?")
+
+## Subset of filtered data with related days.
+datos<- subset(datos, (datos$Date == "1/2/2007" | datos$Date== "2/2/2007")) 
+
+## Convert to date and time
+datos$DateTime <- strptime(paste(datos$Date, datos$Time), '%d/%m/%Y %H:%M:%S')
+
+## Plotting
+
+plot( datos$DateTime, datos$Sub_metering_1, xlab="", ylab="Energy sub metering", type="l")             
+lines(datos$DateTime, datos$Sub_metering_2, xlab="", ylab="Energy sub metering", type="l", col="Red")  
+lines(datos$DateTime, datos$Sub_metering_3, xlab="", ylab="Energy sub metering", type="l", col="Blue") 
+## Legend
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, col=c("black", "red", "blue"))
+
+## Saving the file
+dev.copy(png, file="plot3.png")
+
+## Closing device
+dev.off()
